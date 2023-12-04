91030111925ccae2b530d52b507c2d4c641129db
feruzbekqahramov <feqahramnov@mail.com>
2023-12-04 18:27:55 +0500

    vazifa bajarildi

---
+++ js/index.js
@@ -0,0 +1,118 @@
+// 1-masala
+// let n = +prompt('Sonni kiriting !');
+// for (let i = 1; i<=n; i += 2) {
+//     if (n % 2 == 1) {
+//         console.log(sum)
+//     }
+// }
+
+// 2-masala
+// let n = +prompt('Ihtiyoriy son kiriting!');
+// let sum = 1;
+// for(let i = n; i >= 1; i--) {
+//     if (i % 3 == 0) {
+//         sum *= i;
+//     }
+// }
+// console.log(sum);
+
+// 3-masala
+// let a = +prompt('Birinchi sonni kiriting');
+// let b = +prompt('ikkinchi sonni kiriting');
+// let sum = 0;
+// for(let i = a; i <= 1; i += 2) {
+//     if (i % 3 == 0) {
+//     }
+// }
+// for(let i = b; i <= 1; i += 2) {
+//     if (i % 3 == 0) {
+//     }
+// }
+// console.log(sum);
+
+// 4-masala
+// let a = +prompt('Birinchi sonni kiriting');
+// let b = +prompt('ikkinchi sonni kiriting');
+// let sum = 0;
+// for(let  i = a; i <= b; i++) {
+//     if (i % 5 == 0 && i % 7 == 0)
+//     sum += i;
+// }
+// console.log(sum)
+
+// 5-masala
+// let a = +prompt('Birinchi sonni kiriting');
+// let b = +prompt('ikkinchi sonni kiriting');
+// let sum = 0;
+//  for(let  i = a; i >= b; i++) {
+//      if (i % 3 == 0 && i * 2)
+//      sum += i;
+// }
+// console.log(sum)
+
+// 6-masala
+// let b = +prompt('Birinchi sonni kiriting');
+// let a = +prompt('ikkinchi sonni kiriting');
+// let sum = 0;
+//  for(let  i = b; i >= a; i++) {
+//      if (i % 2 == 0 && i * 3)
+//      sum += i;
+// }
+// console.log(sum)
+
+// 7-masala
+// let a = +prompt('Sonni kiriting');
+// let sum = 0;
+// for(let i = 1; i <= son; i++) {
+//     if (son % i === 0) {
+//         console.log(i)
+//     }
+// }
+
+// 8-masala 
+// let son = 20;
+// let boluvchilar = [];
+// console.log('Siz kiritgan sonni bo`luvchilari');
+// for (let i = 1; i <= son; i++) {
+//   if (son % i === 0) {
+//     boluvchilar.push(i);
+//   }
+// }
+// console.log(boluvchilar);
+
+// 9-masala
+// let a = +prompt('Sonni kiriting');
+// let son = 17;
+// let tub = true;
+// if (son <= 1) {
+//   tub = false;
+// } else {
+//   for (let i = 2; i <= Math.sqrt(son); i++) {
+//     if (son % i === 0) {
+//       tub = false;
+//       break;
+//     }
+//   }
+// }
+// if (tub) {
+//   console.log(`Siz kiritgan son tub`);
+// } else {
+//   console.log(`Siz kiritgan son tub emas`);
+// }
+
+// 10-masala
+// var userInput = prompt("Foydalanuvchi ma'lumotlarini kiriting (a b:");
+// var inputArray = userInput.split(" ");
+
+// var a = parseInt(inputArray[0]);
+// var b = parseInt(inputArray[1]);
+
+// var jarimaXato = a * 5;
+
+// var jarimaTogri = mb
+
+// var yigindi = jarimaXato + jarimaTogri;
+
+// console.log(yigindi);
+
+// 11-masala
